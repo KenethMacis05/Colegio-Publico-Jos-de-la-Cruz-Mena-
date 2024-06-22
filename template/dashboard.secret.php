41126133c9c00f41e287d65389c914ae9c5042d2
keny <ken123oficial@gmail.com>
2024-06-22 17:52:54 -0600

    BOA II

--- template/dashboard.secret.php
+++ template/dashboard.secret.php
@@ -1,6 +1,6 @@
 
 <!--Aqui comienza-->
-<div class="col-auto col-md-3 col-xl-2 px-sm-2 bg-dark p-3 text-white contenedor" style="width: 280px; height: 100%; background-color: rgba(33,37,41,0.9);">
+<div class="col-auto col-md-3 col-xl-2 px-sm-2 bg-dark p-3 text-white contenedor" style="width: 280px; height: 100%;">
 
     <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
         <a href="/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
